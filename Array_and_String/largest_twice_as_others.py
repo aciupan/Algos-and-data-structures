a08e61549eb0fd346ef4d33ded412023621b574f
aciupan <andrei.ciupan@gmail.com>
2019-09-22 15:49:13 -0700

    Create largest_twice_as_others.py

---
+++ Array_and_String/largest_twice_as_others.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1147/
+class Solution:
+    def dominantIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_nr = -math.inf
+        max_2nd = -math.inf
+        index = -1
+        if len(nums) == 0:
+            return -1
+        for i, x in enumerate(nums):
+            if x > max_nr:
+                max_2nd = max_nr
+                max_nr = x
+                index = i
+            elif x> max_2nd:
+                max_2nd = x
+        if max_nr >= 2*max_2nd:
+            return index
+        return -1
