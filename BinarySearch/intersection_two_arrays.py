788a884fd943400d2523ad8ff11a8b1ab0968b72
aciupan <andrei.ciupan@gmail.com>
2019-09-22 14:16:13 -0700

    Create intersection_two_arrays.py

---
+++ BinarySearch/intersection_two_arrays.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/explore/learn/card/binary-search/144/more-practices/1034/
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1) & set(nums2))
