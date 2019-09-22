5ac73a0b8c695a83ca103c6fca1f6691a97154e3
aciupan <andrei.ciupan@gmail.com>
2019-09-22 15:39:58 -0700

    Create array_intersection.py

---
+++ Hashes/array_intersection.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/explore/learn/card/hash-table/183/combination-with-other-algorithms/1105/
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1) & set(nums2))
+        
