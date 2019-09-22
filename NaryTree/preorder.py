b2f1091112ae00061da1db8316b32de14189816d
aciupan <andrei.ciupan@gmail.com>
2019-09-22 15:35:02 -0700

    Create preorder.py

---
+++ NaryTree/preorder.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/explore/learn/card/n-ary-tree/130/traversal/925/
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if root == None:
+            return []
+        sol = [root.val]
+        for child in root.children:
+            sol += self.preorder(child)
+        return sol
