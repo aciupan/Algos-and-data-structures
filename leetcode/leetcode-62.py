e04c79277d734b37f0e170c49ffdab8a6a942a7e
aciupan <andrei.ciupan@gmail.com>
2020-03-17 18:30:21 -0400

    Create leetcode-62.py

---
+++ leetcode/leetcode-62.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/unique-paths/
+class Solution:
+    def n_choose_k(self, n, k):
+        if k == 0:
+            return 1
+        return self.n_choose_k(n, k-1) * (n-k+1) // k
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m <=n:
+            return self.n_choose_k(m+n-2, m-1)
+        return self.n_choose_k(m+n-2, n-1)
